5a008afacf9da9248cf83ab946a3b2f312dd3469
Erick Kaique da Silva <erick.silva@c6bank.com>
2019-09-10 14:12:50 -0300

    Atualização Site

--- greencode-web/src/ui/slider.js
+++ greencode-web/src/ui/slider.js
@@ -4,8 +4,18 @@ export default class Slide extends Component {
     render() {
         return (
             <>
-                <div className="container-fluid">
-                    <h1>AQUI UM SLIDE SHOW</h1>
+                <div className="container-fluid wallpaper-homes">
+                    <div className="row">
+                        <div className="col-lg-1"></div>
+                        <div className="col-lg-6">
+                            <h1 className="title-big">What is Lorem Ipsum</h1>
+                            <p className="description">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                            </p>
+                            <button type="button" class="btn btn-lg button-big">Botão primário</button>
+                        </div>
+                    </div>
                 </div>
             </>
         );
