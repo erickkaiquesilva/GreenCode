1ff031c153b92eaa6c3908ddee2c1ac81a099b98
Erick Kaique da Silva <erick.silva@c6bank.com>
2019-09-12 19:55:22 -0300

    Atualizando Site Institucional

--- greencode-web/src/index.js
+++ greencode-web/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './scenes/App';
 import * as serviceWorker from './serviceWorker';
+import {Router, Route, hasHistory} from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router history={hasHistory}>
+        <Route path='/' component={App} />
+    </Router>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
