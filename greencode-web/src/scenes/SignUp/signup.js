a6ffd48425e52a2cad3c47eb6101116eb275805b
Erick Kaique da Silva <erick.silva@c6bank.com>
2019-10-06 14:47:59 -0300

    Criando Tela de Cadastro

---
+++ greencode-web/src/scenes/SignUp/signup.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import NavBar from '../../Components/NavBar/navbar'
+
+export default class SignUp extends Component {
+    render(){
+        return(
+            <>
+                <NavBar />
+                <div className="container">
+                    <div className="row">
+                        <article className="conteudo-esquerda col-lg-6">
+                            <h1>Here you recycle in a fun way!</h1>
+                            <div className="borda-separadora"></div>
+                            <p>
+                                Do not have an account yet ? so sign up now and start changing the world in a fun way.
+                            </p>
+                        </article>
+                        <article className="col-lg-6">
+                            <div className="formulario-login">
+                                <h1>Sign Up</h1>
+                                <form>
+                                    <input className="caixadeentrada" type="text" placeholder="Nome Completo" />
+                                    <input className="caixadeentrada" type="email" placeholder="Email de Acesso" />
+                                    <input className="caixadeentrada" type="password" placeholder="Senha" />
+                                    <input className="caixadeentrada" type="password" placeholder="Confirme Senha" />
+                                    <input type="submit" value="Login" />
+                                </form>
+                            </div>
+                        </article>
+                    </div>
+                    <div className="espaco"></div>
+                    <div className="espaco"></div>
+                </div>
+            </>
+        )
+    }
+}
