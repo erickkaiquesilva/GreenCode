adee8f535ab7a2562d612dce5c2f70138cdcaa0b
italo-andrade-99 <italo.andrade@bandtec.com.br>
2019-10-22 18:27:26 -0300

    Usuario Gastando Pontos

--- greencode-back-end/src/main/java/com/greencode/demo/controller/UsuarioController.java
+++ greencode-back-end/src/main/java/com/greencode/demo/controller/UsuarioController.java
@@ -3,11 +3,13 @@ package com.greencode.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.greencode.demo.domain.UsuariosRepository;
+import com.greencode.demo.domain.Produto;
 import com.greencode.demo.domain.Usuario;
 
 @RestController
@@ -29,5 +31,26 @@ public class UsuarioController {
 		
 		tds.save(usuario);
 		return ResponseEntity.ok(true);
+	}
+	
+	@PostMapping("/usuario/gastar/{id}")
+	public ResponseEntity<Integer> gastar(@RequestBody Produto produto, @PathVariable String id){
+		Long usuarioId = Long.parseLong(id);
+		int pontosAtuais = tds.buscarPontosPorId(usuarioId);
+		int pontosAtualizados;
+		
+		if(pontosAtuais > produto.getPreco()) {
+			pontosAtualizados = (int) pontosAtuais - (int) produto.getPreco();
+			
+			tds.atualizarPontos(pontosAtualizados, usuarioId);
+			return ResponseEntity.ok(pontosAtualizados);
+		}
+		else {
+			return ResponseEntity.ok(pontosAtuais);
+		}
+		
+		
+		
+		
 	}
 }
---
+++ greencode-back-end/src/main/java/com/greencode/demo/domain/ProdutoRepository.java
@@ -0,0 +1,21 @@
+package com.greencode.demo.domain;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	@Query("select p from Produto p where p.nome = :nome")
+	public List<Produto> porNome(@Param("nome") String nome);
+	
+	@Transactional
+	@Modifying
+	@Query("update Produto set preco = :preco where id = :id")
+	public void atualizarPreco (@Param("preco") Double preco,@Param("id") Long id);
+}
