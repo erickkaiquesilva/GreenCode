3826154a5c227948423e1b39ce5fc6dd31832770
Gustavo Barbosa <gustavo.barbosa@nubeliu.com>
2019-11-06 08:49:30 -0300

    modificação metodo

--- greencode-back-end/src/main/java/com/greencode/demo/dao/RecicladoRepository.java
+++ greencode-back-end/src/main/java/com/greencode/demo/dao/RecicladoRepository.java
@@ -1,5 +1,7 @@
 package com.greencode.demo.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,5 +10,5 @@ import com.greencode.demo.model.Reciclados;
 public interface RecicladoRepository extends JpaRepository<Reciclados, Long> {
 	
 	//@Query("select r from r where r.id := id")
-	public Reciclados findById(Long id);
+	public Optional<Reciclados> findById(Long id);
 }
